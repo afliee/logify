5f69f1e0a37afe14e01d2672437951137a042baa
Kunzzz <quoctruong310803@gmail.com>
2023-04-24 00:30:33 +0700

    feat: change rules sign in and add navigation bar

--- app/src/main/java/com/example/logify/MainActivity.java
+++ app/src/main/java/com/example/logify/MainActivity.java
@@ -1,48 +1,119 @@
 package com.example.logify;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-
+import com.example.logify.auth.SignInActivity;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
-    TextView userName;
-    Button btnLogout;
-    String name = "";
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth mAuth;
+    private BottomNavigationView.OnItemSelectedListener onItemSelectedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
 
-        userName = findViewById(R.id.userName);
-        btnLogout = findViewById(R.id.btnLogout);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        mAuth = FirebaseAuth.getInstance();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth mAuth = FirebaseAuth.getInstance();
-                mAuth.signOut();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.e(TAG, "onNavigationItemSelected: bottom item " + item.getTitle() + " clicked");
+                switch (item.getItemId()) {
+                    case R.id.bottom_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bottom_search:
+                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bottom_playlist:
+                        Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bottom_profile:
+                        Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
-
     }
 
-    private void getDataIntent() {
-        Intent intent = getIntent();
-        if (intent != null) {
-            name = intent.getStringExtra("username");
-            userName.setText(name);
+
+    private  void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+//    out of onCreate
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_about:
+                break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                SharedPreferences userRef = getSharedPreferences("user", MODE_PRIVATE);
+                SharedPreferences.Editor editor = userRef.edit();
+                editor.clear();
+                editor.apply();
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
--- app/src/main/java/com/example/logify/SplashActivity.java
+++ app/src/main/java/com/example/logify/SplashActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.example.logify.auth.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
     FirebaseAuth mAuth;
 
     @Override
@@ -28,6 +30,8 @@ public class SplashActivity extends AppCompatActivity {
 //                finish();
                 SharedPreferences preferences = getSharedPreferences("onBoardingScreen", MODE_PRIVATE);
                 boolean isFirstTime = preferences.getBoolean("firstTime", true);
+                SharedPreferences uuidPref = getSharedPreferences("user", MODE_PRIVATE);
+                String uuid = uuidPref.getString("uuid", null);
                 if (isFirstTime) {
                     SharedPreferences.Editor editor = preferences.edit();
                     editor.putBoolean("firstTime", false);
@@ -37,11 +41,18 @@ public class SplashActivity extends AppCompatActivity {
                     finish();
                 } else {
 //                    check if user was signed in
-                    if (mAuth.getCurrentUser() != null) {
+                    if (uuid != null) {
+                        Log.e(TAG, "run: uuid found " + uuid);
+                        if (mAuth.getCurrentUser() == null) {
+                            Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
                         Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
                     } else {
+                        Log.e(TAG, "run: uuid not found " + uuid);
                         Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
                         startActivity(intent);
                         finish();
