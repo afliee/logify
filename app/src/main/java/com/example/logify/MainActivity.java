4bb75236ddec4d8002188d32541d00d418c698fa
Kunzzz <quoctruong310803@gmail.com>
2023-04-24 14:48:13 +0700

    choke: setup fragment interaction

--- app/src/main/java/com/example/logify/MainActivity.java
+++ app/src/main/java/com/example/logify/MainActivity.java
@@ -18,10 +18,19 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.logify.auth.SignInActivity;
+import com.example.logify.fragments.AboutUsFragment;
+import com.example.logify.fragments.HomeFragment;
+import com.example.logify.fragments.PlaylistFragment;
+import com.example.logify.fragments.ProfileFragment;
+import com.example.logify.fragments.SearchFragment;
+import com.example.logify.fragments.SettingFragment;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.time.LocalTime;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
@@ -41,6 +50,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mAuth = FirebaseAuth.getInstance();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+//        set title action bar
+        String titleApp = "";
+//        check date is morning or afternoon or event and set title
+        LocalTime time = LocalTime.now();
+        if (time.isAfter(LocalTime.of(5, 0)) && time.isBefore(LocalTime.of(12, 0))) {
+            titleApp = "Good Morning";
+        } else if (time.isAfter(LocalTime.of(12, 0)) && time.isBefore(LocalTime.of(18, 0))) {
+            titleApp = "Good Afternoon";
+        } else {
+            titleApp = "Good Evening";
+        }
+        getSupportActionBar().setTitle(titleApp);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
                 R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
@@ -58,16 +82,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Log.e(TAG, "onNavigationItemSelected: bottom item " + item.getTitle() + " clicked");
                 switch (item.getItemId()) {
                     case R.id.bottom_home:
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new HomeFragment());
+//                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.bottom_search:
-                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new SearchFragment());
+//                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.bottom_playlist:
-                        Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new PlaylistFragment());
+//                        Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.bottom_profile:
-                        Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new ProfileFragment());
+//                        Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
                         return true;
                     default:
                         return false;
@@ -88,10 +116,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
+                replaceFragment(new HomeFragment());
                 break;
             case R.id.nav_settings:
+                replaceFragment(new SettingFragment());
                 break;
             case R.id.nav_about:
+                replaceFragment(new AboutUsFragment());
                 break;
             case R.id.nav_logout:
                 mAuth.signOut();
