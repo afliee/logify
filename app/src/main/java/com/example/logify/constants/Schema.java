a35e3c1bef090a54a80f6526a47a716c303fe448
Kunzzz <quoctruong310803@gmail.com>
2023-05-03 20:28:12 +0700

    choke: push spread

--- app/src/main/java/com/example/logify/constants/Schema.java
+++ app/src/main/java/com/example/logify/constants/Schema.java
@@ -10,9 +10,11 @@ public class Schema {
     public static final String FAVORITE_ALBUMS = "favoriteAlbums";
     public static final String FAVORITE_SONGS = "favoriteSongs";
 
-    public enum PlaylistType {
-        ID,
-        NAME,
-        DESCRIPTION,
+    public static class SongType {
+        public static final String ID = "id";
+        public static final String NAME = "name";
+        public static final String DESCRIPTION = "description";
+        public static final String DURATION = "duration";
+        public static final String IMAGE = "image";
     }
 }
